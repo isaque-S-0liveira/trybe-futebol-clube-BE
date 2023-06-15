a0f7dd344875ba7c56dd5f5e5c76b694d3ab69ca
Isaque <zazac3179@gmail.com>
2023-06-15 17:30:09 -0300

    cobertura total de testes das rotas de login

--- app/backend/src/tests/User.test.ts
+++ app/backend/src/tests/User.test.ts
@@ -1,5 +1,6 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as jwt from 'jsonwebtoken';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -7,7 +8,8 @@ import { app } from '../app';
 import SequelizeUser from '../database/models/UserModel';
 
 import { Response } from 'superagent';
-import {invalidFormatEmail, invalidFormatPassword, invalidPassword, userRegistered, validLoginBody} from './mocks/users.mock'
+import {invalidFormatEmail, invalidFormatPassword, invalidPassword, user, userRegistered, validLoginBody, validToken, verifyValid} from './mocks/users.mock'
+import TokenGeneratorJwt from '../auth/TokenGeneratorJwt';
 
 chai.use(chaiHttp);
 
@@ -69,5 +71,56 @@ describe('Users Test', () => {
     expect(status).to.equal(401);
     expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
   });
+
+  describe('GET /login/role', () => {
+   it('deve retornar a role do usuario caso o token seja valido', async () => {
+
+      sinon.stub(jwt, 'verify').returns({role: 'admin'} as any);
+      const res = await chai.request(app)
+      .get('/login/role')
+      .set('authorization', 'token_valido')
+      console.log(res.body);
+      
+    expect(res).to.have.status(200);
+    expect(res.body).to.deep.equal({role: 'admin'});
+    });
+
+  it('deve retornar o payload do token se o token for válido', () => {
+    sinon.stub(jwt, 'sign').returns(validToken as any);
+    sinon.stub(jwt, 'verify').returns(verifyValid as any)
+    const result = { id: 1, role: 'admin', iat: 1686850644, exp: 1687282644 };
+
+    expect(result).to.deep.equal(verifyValid);
+  });
+
+  it('deve retornar falso se o token for inválido', () => {
+    const invalidToken = 'token inválido';
+
+    const result = TokenGeneratorJwt.verify(invalidToken);
+
+    expect(result).to.be.false;
+  });
+  
+  it('deve retornar uma messagem de erro caso o token não exista', async () => {
+    sinon.stub(TokenGeneratorJwt, 'verify').resolves(null);
+
+    const res = await chai.request(app)
+    .get('/login/role')
+    .set('authorization', '')
+
+    expect(res).to.have.status(401)
+    expect(res.body).to.deep.equal({ message: 'Token not found' })
+  })
+  it('deve retornar uma messagem de erro caso o token seja invalido', async () => {
+    sinon.stub(TokenGeneratorJwt, 'verify').resolves(null);
+
+    const res = await chai.request(app)
+    .get('/login/role')
+    .set('authorization', 'token_invalido')
+
+    expect(res).to.have.status(401)
+    expect(res.body).to.deep.equal({ message: 'Token must be a valid token' })
+  })
+});
   afterEach(sinon.restore);
 });
--- app/backend/src/tests/mocks/users.mock.ts
+++ app/backend/src/tests/mocks/users.mock.ts
@@ -12,6 +12,8 @@ const userRegistered = { ...user, password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0
 const invalidFormatPassword = { email: 'jondoe@email.com', password: 'x' };
 const invalidFormatEmail = { email: 'invalid_email', password: '123456' };
 const invalidPassword = { email: 'admin@admin.com', password: 'invalid' };
+const validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjg2ODUwNjQ0LCJleHAiOjE2ODcyODI2NDR9.raCB6TSd_4CKJEwb0BeTcAs2gXcq18BsGR5b9HkJonM";
+const verifyValid = { id: 1, role: 'admin', iat: 1686850644, exp: 1687282644 }
 
 export {
     validLoginBody,
@@ -19,4 +21,7 @@ export {
     invalidFormatEmail,
     invalidFormatPassword,
     invalidPassword,
+    validToken,
+    user,
+    verifyValid
 }
