50200bd3199e6051c9156a337012e1e1838f1fc1
Isaque <zazac3179@gmail.com>
2023-06-15 22:45:08 -0300

    criação de teste do end point /matches + preparação do ambiente de teste para futuras implementações

---
+++ app/backend/src/tests/Matches.test.ts
@@ -0,0 +1,137 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import SequelizeMatche from '../database/models/MatcheModel';
+
+import { Response } from 'superagent';
+import { matches, matchesResult } from './mocks/matches.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Matches Test', () => {
+    describe('GET /matches', () => {
+
+  it('Deve retornar todas as partidas', async () => {
+
+     sinon.stub(SequelizeMatche, 'findAll').resolves(matchesResult as any);
+
+    const { status, body } = await chai.request(app).get('/matches');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matchesResult);
+  });
+  it('deve ser possivel filtrar somente partidas em andamento ou partidas finalizadas', async () => {
+
+    // sinon.stub(SequelizeTeam, 'findByPk').resolves(teamsMock.teams[0] as any);
+
+    // const res = await chai.request(app).get('/teams/1');
+
+    // expect(res.status).to.equal(200);
+    // expect(res.body).to.deep.equal(teamsMock.teams[0]);
+});
+});
+  it('deve ser possivel finalizar uma partida no DB', async () => {
+
+//     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+  
+//     const {status, body} = await chai.request(app).get('/teams/99');
+  
+//     expect(status).to.equal(404);
+//     expect(body).to.deep.equal({ message: 'Team not found' });
+});
+
+it(' não deve ser possivel finalizar uma partida no DB quando o token não existir', async () => {
+
+    //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+      
+    //     const {status, body} = await chai.request(app).get('/teams/99');
+      
+    //     expect(status).to.equal(404);
+    //     expect(body).to.deep.equal({ message: 'Team not found' });
+    });
+    it('não deve ser possivel finalizar uma partida no DB quando o token for invalido', async () => {
+
+        //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+          
+        //     const {status, body} = await chai.request(app).get('/teams/99');
+          
+        //     expect(status).to.equal(404);
+        //     expect(body).to.deep.equal({ message: 'Team not found' });
+        });
+
+ it('deve ser possivel atualizar partidas em andamento', async () => {
+
+    //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+      
+    //     const {status, body} = await chai.request(app).get('/teams/99');
+      
+    //     expect(status).to.equal(404);
+    //     expect(body).to.deep.equal({ message: 'Team not found' });
+    });
+
+ it('não deve ser possivel atualizar partidas em andamento quando o token não é informado', async () => {
+
+        //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+          
+        //     const {status, body} = await chai.request(app).get('/teams/99');
+          
+        //     expect(status).to.equal(404);
+        //     expect(body).to.deep.equal({ message: 'Team not found' });
+     });
+     it('não deve ser possivel atualizar partidas em andamento quando o token não é valido', async () => {
+
+        //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+          
+        //     const {status, body} = await chai.request(app).get('/teams/99');
+          
+        //     expect(status).to.equal(404);
+        //     expect(body).to.deep.equal({ message: 'Team not found' });
+        });
+
+        it('deve ser possivel cadastrar uma nova partida', async () => {
+
+            //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+              
+            //     const {status, body} = await chai.request(app).get('/teams/99');
+              
+            //     expect(status).to.equal(404);
+            //     expect(body).to.deep.equal({ message: 'Team not found' });
+      });
+
+      it('não deve ser possivel cadastrar uma nova partida caso  não exista um token', async () => {
+
+        //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+          
+        //     const {status, body} = await chai.request(app).get('/teams/99');
+          
+        //     expect(status).to.equal(404);
+        //     expect(body).to.deep.equal({ message: 'Team not found' });
+        });
+
+        it('não deve ser possivel cadastrar uma nova partida caso o token seja invalido', async () => {
+
+            //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+              
+            //     const {status, body} = await chai.request(app).get('/teams/99');
+              
+            //     expect(status).to.equal(404);
+            //     expect(body).to.deep.equal({ message: 'Team not found' });
+        });
+
+        it('não deve ser possivel cadastrar uma nova partida com times iguais nem com um time que não existe na tabela de times', async () => {
+
+            //     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+              
+            //     const {status, body} = await chai.request(app).get('/teams/99');
+              
+            //     expect(status).to.equal(404);
+            //     expect(body).to.deep.equal({ message: 'Team not found' });
+        });
+
+  afterEach(sinon.restore);
+});
---
+++ app/backend/src/tests/mocks/matches.mock.ts
@@ -0,0 +1,54 @@
+const matches =  [
+    {
+      id: 1,
+      home_team_id: 16,
+      home_team_goals: 1,
+      away_team_id: 8,
+      away_team_goals: 1,
+      in_progress: false,
+    },
+    {
+      id: 41,
+      homeTeamId: 16,
+      homeTeamGoals: 2,
+      awayTeamId: 9,
+      awayTeamGoals: 0,
+      inProgress: true,
+    }
+];
+
+const matchesResult = [
+    {
+      "id": 1,
+      "homeTeamId": 16,
+      "homeTeamGoals": 1,
+      "awayTeamId": 8,
+      "awayTeamGoals": 1,
+      "inProgress": false,
+      "homeTeam": {
+        "teamName": "São Paulo"
+      },
+      "awayTeam": {
+        "teamName": "Grêmio"
+      }
+    },
+    {
+      "id": 41,
+      "homeTeamId": 16,
+      "homeTeamGoals": 2,
+      "awayTeamId": 9,
+      "awayTeamGoals": 0,
+      "inProgress": true,
+      "homeTeam": {
+        "teamName": "São Paulo"
+      },
+      "awayTeam": {
+        "teamName": "Internacional"
+      }
+    }
+  ]
+
+export {
+    matches,
+    matchesResult,
+};
